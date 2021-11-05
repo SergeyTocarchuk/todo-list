15fc73bdc9b4b93e778df1859b5f652d7a8aa52b
Serhii T <tocarchuk@gmail.com>
2021-11-05 13:38:07 +0200

    Add Input Task form

--- src/index.js
+++ src/index.js
@@ -7,3 +7,49 @@ openNavButton.addEventListener('click', openNav);
 function openNav(){
   openNavButton.classList.toggle('active');
 }
+
+const btnAddTask = document.getElementById('button-add-task');
+btnAddTask.addEventListener('click', createTask);
+
+
+
+function createTask(){
+  const taskList = document.getElementById('task-list');
+  taskList.innerHTML = `
+  <div class="add-task-popup" id="add-task-popup">
+    <input
+      class="input-add-task-popup"
+      id="input-add-task-popup"
+      type="text"
+    />
+    <div class="add-task-popup-buttons">
+      <button class="button-add-task-popup" id="button-add-task-popup">
+        Add
+      </button>
+      <button
+        class="button-cancel-task"
+        id="button-cancel-task"
+        >
+        Cancel
+      </button>
+    </div>
+  </div>`
+  openAddTaskPopup();
+
+  const btnCancelTask = document.getElementById('button-cancel-task');
+  btnCancelTask.addEventListener('click', closeAddTaskPopup);
+}
+
+function openAddTaskPopup() {
+  const addTaskPopup = document.getElementById('add-task-popup');
+  addTaskPopup.classList.add('active');
+
+  btnAddTask.classList.add('hide');
+}
+
+function closeAddTaskPopup() {
+  const addTaskPopup = document.getElementById('add-task-popup');
+  addTaskPopup.classList.remove('active');
+  
+  btnAddTask.classList.remove('hide');
+}
