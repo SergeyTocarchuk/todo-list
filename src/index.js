35aa921819fe6e07a64d880683576143412dfc0f
Serhii T <tocarchuk@gmail.com>
2021-11-08 20:59:08 +0200

    Add annimation with CSS to Navbar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import _ from 'lodash';
 import './style.css';
+// import Task from './task';
 
-// const openNavButton = document.getElementById('btn-open-nav');
-// openNavButton.addEventListener('click', openNav);
+const openNavButton = document.getElementById('btn-open-nav');
+openNavButton.addEventListener('click', displayTasksList);
+
+const inboxButton = document.getElementById('show-list');
+inboxButton.addEventListener('click', displayTasksList);
+
+const addTaskNav = document.getElementById('nav-add-task');
+addTaskNav.addEventListener('click', openAddTaskPopup);
 
 // function openNav(){
 //   openNavButton.classList.toggle('active');
@@ -24,6 +31,7 @@ btnCancelTask.addEventListener('click', closeAddTaskPopup);
 function addTask() {
   const inputAddTask = document.getElementById('input-add-task');
   addTaskToLocalStorage(inputAddTask.value);
+  closeAddTaskPopup();
 }
 
 function openAddTaskPopup() {
@@ -44,8 +52,31 @@ function addTaskToLocalStorage(task) {
     window.localStorage.setItem("localStorageTasks", JSON.stringify(myTasks));
   } else {
     myTasks = [];
-    myTasks.push(book);
+    myTasks.push(task);
     window.localStorage.setItem("localStorageTasks", JSON.stringify(myTasks));
   }
+  displayTasksList();
   console.log(myTasks);
+}
+
+function displayTasksList() {  
+  for( let i = 0; i < myTasks.length; i++ ){
+    console.log(myTasks[i]);
+    const tasksList = document.getElementById('task-list');
+    tasksList.innerHTML = 
+        `<span>${myTasks[i]}</span>
+      <button class="button-task" data-task-button>
+        <div class="left-task-panel">
+          <i class="far fa-circle"></i>
+          <p class="task-content">${myTasks[i]}</p>
+          <input type="text" class="input-task-name" data-input-task-name>
+        </div>
+        <div class="right-task-panel">
+          <p class="due-date" id="due-date">${myTasks[i+1]}</p>
+          <input type="date" class="input-due-date" data-input-due-date>
+          <i class="fas fa-times"></i>
+        </div>
+      </button>
+    `
+  }
 }
